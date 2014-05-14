86cfb587efd9b46560f1e894001b319c063fb815
Antonio Fazari <antonio.fazari88@gmail.com>
2014-05-14 12:11:30 +0100

    Initial Commit

---
+++ todo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  todo
+//
+//  Created by Antonio Fazari on 14/05/2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "todoAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([todoAppDelegate class]));
+    }
+}
---
+++ todo/todoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  todoAppDelegate.h
+//  todo
+//
+//  Created by Antonio Fazari on 14/05/2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface todoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
